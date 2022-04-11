4a2bdd05dc9b3d6e8cb27b19b907aae8cdb9336d
hemraj0906 <hemrajdeshmukh0906@gmail.com>
2022-04-10 20:30:01 -0700

    create new player access body

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,53 @@
-let data = req.body
-console.log(data)
-players.map((x) => {
-    if (x.name == data.name) {
-        return res.send("player already exist")
-    }
+const { request } = require('express');
+const express = require('express');
+const router = express.Router();
+ 
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ],
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ],
+       },
+   ]
+ 
+   router.post('/players', function (req, res) {
+ 
+       //LOGIC WILL COME HERE
+   
+       let data = req.body
+       console.log(data)
+       players.map((x) => {
+           if (x.name == data.name) {
+               return res.send("player already exist")
+           }
+   
+       })
+       players.push(data)
+       res.send({ msg: "My NewPlayerList:" , Player : players })
 
-})
-players.push(data)
-res.send({ msg: "My NewPlayerList:" , Player : players })
-
-
-
- module.exports = router;
+   })
+  
+module.exports = router;
